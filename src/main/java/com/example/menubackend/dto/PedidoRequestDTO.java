0b9be9eebe910500862aeea2c7cc87ad5030c4b3
Marcelo Moro <101741773+marcelomoro1@users.noreply.github.com>
2025-08-21 10:11:57 -0300

    PedidoRequestDTO.java

--- src/main/java/com/example/menubackend/dto/PedidoRequestDTO.java
+++ src/main/java/com/example/menubackend/dto/PedidoRequestDTO.java
@@ -2,9 +2,7 @@ package com.example.menubackend.dto;
 
 import lombok.Data;
 
-// Neste cenário, o PedidoRequestDTO pode ser vazio ou ter um campo para observações, se necessário.
-// A lógica de criação do pedido virá do carrinho associado ao usuário logado.
 @Data
 public class PedidoRequestDTO {
-    // String observacoes; // Exemplo de campo opcional para o cliente adicionar
+
 }
